c43cb94caca2628b9b5dd1bf3c6ebe7fdea281c1
Andrei Lyakh <Edgar.digl@mail.ru>
2026-03-30 15:21:13 +0300

    Filters,search and ordering have been created.

--- events/views.py
+++ events/views.py
@@ -1,3 +1,5 @@
+from re import search
+
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -7,12 +9,55 @@ from bookings.serializers import BookingSerializer
 from .models import Event, Review
 from .serializers import EventSerializer, ReviewSerializer, EventRetrieveSerializer
 
-
 # Event viewset
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    ALLOWED_ORDERING_FIELDS = ['start_date', '-start_date','end_date', '-end_date', 'price', '-price']
+
+    # override list action
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+
+        # filters
+        queryset = self._apply_filters(request, queryset)
+
+        # ordering
+        queryset = self._apply_ordering(request, queryset)
+
+        return Response(EventSerializer(queryset, many=True).data)
+
+    def _apply_ordering(self, request, queryset):
+        ordering = request.GET.get('ordering', None)
+
+        if ordering and ordering in self.ALLOWED_ORDERING_FIELDS:
+            queryset = queryset.order_by(ordering)
+
+        return queryset
+
+    def _apply_filters(self, request, queryset):
+        date_from, date_to = request.GET.get('date_from', None), request.GET.get('date_to', None)
+        organizer = request.GET.get('organizer', None)
+        price_min, price_max = request.GET.get('price_min', None), request.GET.get('price_max', None)
+        search = request.GET.get('search', None)
+
+        # filters
+        if date_from:
+            queryset = queryset.filter(start_date__gte=date_from)
+        if date_to:
+            queryset = queryset.filter(end_date__lte=date_to)
+        if organizer:
+            queryset = queryset.filter(organizer=organizer)
+        if price_min:
+            queryset = queryset.filter(price__gte=price_min)
+        if price_max:
+            queryset = queryset.filter(price__lte=price_max)
+        if search:
+            queryset = queryset.filter(title__icontains=search)
+
+        return queryset
 
     # Override serializer getting
     def get_serializer_class(self):
@@ -22,8 +67,8 @@ class EventViewSet(viewsets.ModelViewSet):
             return EventSerializer
 
     @action(detail=True, methods=['get'], url_path='reviews')
-    def get_all_reviews(self, request, pk=None):
-        reviews = Review.objects.filter(event=pk).all()
+    def reviews(self, request, pk=None):
+        reviews = Review.objects.filter(event=pk)
         return Response(ReviewSerializer(instance=reviews, many=True).data)
 
     @action(detail=True, methods=['post'], url_path='book')
