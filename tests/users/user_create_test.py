8f6ad7991abb0c88897cba57ebd83e83d67a919c
Andrei Lyakh <Edgar.digl@mail.ru>
2026-03-31 10:13:21 +0300

    Created users tests

--- tests/users/user_create_test.py
+++ tests/users/user_create_test.py
@@ -1,27 +1,49 @@
 import pytest
+from rest_framework.exceptions import ErrorDetail
+
 
 # test for /users/create/
 @pytest.mark.django_db
-def test_create_user(client):
-    expected_data = {
-        'id': 1,
-        'username': 'new_test_user',
-        'email': '',
-        'phone_number': '+375292847284',
-        'date_of_birth': '2006-01-26'
-    }
-
-    response = client.post(
-        '/users/create/',
-        data = {
+class TestCreateUser:
+    def test_create_user(self, client):
+        expected_data = {
+            'id': 1,
             'username': 'new_test_user',
             'email': '',
             'phone_number': '+375292847284',
             'date_of_birth': '2006-01-26',
-            'password': 'test_password'
-        },
-        format='json'
-    )
+            'is_organizer': False
+        }
 
-    assert response.status_code == 201
-    assert response.data == expected_data
+        response = client.post(
+            '/users/create/',
+            data = {
+                'username': 'new_test_user',
+                'email': '',
+                'phone_number': '+375292847284',
+                'date_of_birth': '2006-01-26',
+                'password': 'test_password'
+            },
+            format='json'
+        )
+        assert response.status_code == 201
+        assert response.data == expected_data
+
+    def test_invalid_phone(self, client):
+        expected_data = {'phone_number':
+                             [ErrorDetail(string='Incorrect phone number, correct format: +375xxxxxxxxx', code='invalid')]
+                         }
+
+        response = client.post(
+            '/users/create/',
+            data={
+                'username': 'new_test_user',
+                'email': '',
+                'phone_number': '+3752928472846',
+                'date_of_birth': '2006-01-26',
+                'password': 'test_password'
+            },
+            format='json'
+        )
+        assert response.status_code == 400
+        assert response.data == expected_data
